3d5fd986229231963393d49058d96d7fdaa4d412
Pedro Sudario <pedrohenriquesudariosilva@gmail.com>
2024-06-15 22:22:39 -0300

    Most recent posts

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -8,13 +8,45 @@ import { FirebaseService } from 'src/app/services/FirebaseService.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  threads: any[] = [];
   isSignedIn = false;
   constructor(public firebaseService: FirebaseService) {}
 
   ngOnInit() {
     if (localStorage.getItem('user') !== null) this.isSignedIn = true;
     else this.isSignedIn = false;
+    this.fetchThreads();
+  }
+
+  fetchThreads() {
+    this.firebaseService.getDocuments().subscribe((threads: any[]) => {
+      this.threads = threads;
+      this.threads.map((thread) => {
+        let today = new Date();
+        let difference = today.getTime() - thread.date.toDate().getTime();
+
+        let daysDifference = Math.floor(difference / (1000 * 60 * 60 * 24));
+        let hoursDifference = Math.floor(
+          (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        );
+        let minutesDifference = Math.floor(
+          (difference % (1000 * 60 * 60)) / (1000 * 60)
+        );
+        let secondsDifference = Math.floor((difference % (1000 * 60)) / 1000);
+
+        if (daysDifference > 0) {
+          thread.date = daysDifference + ' dia(s) atrás';
+        } else if (hoursDifference > 0) {
+          thread.date = hoursDifference + ' hora(s) atrás';
+        } else if (minutesDifference > 0) {
+          thread.date = minutesDifference + ' minuto(s) atrás';
+        } else if (secondsDifference > 0) {
+          thread.date = secondsDifference + ' segundo(s) atrás';
+        } else {
+          thread.date = 'Agora';
+        }
+      });
+    });
   }
 
 }
