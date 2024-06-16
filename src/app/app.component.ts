3feabb209820219963d48d8af40eb96381d9c067
Pedro Sudario <pedrohenriquesudariosilva@gmail.com>
2024-06-16 16:55:39 -0300

    Application name change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,7 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'bit-threads-nine';
-
+  constructor(private titleService:Title) {
+    this.titleService.setTitle("BitThreads");
+  }
 }
